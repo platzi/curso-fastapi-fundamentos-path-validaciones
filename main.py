3e68433611c1a407560fdbbd435628f3634b238c
Facundo García Martoni <thunderxforce@gmail.com>
2021-08-19 11:23:27 -0300

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+# Models
+
+class Person(BaseModel): 
+    first_name: str
+    last_name: str
+    age: int 
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home(): 
     return {"Hello": "World"}
+
+# Request and Response Body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)): 
+    return person
