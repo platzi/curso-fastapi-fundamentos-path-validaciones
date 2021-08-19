02917803588fca62a7d91e8b153f360949daa82b
Facundo García Martoni <thunderxforce@gmail.com>
2021-08-19 11:54:27 -0300

    validations_query

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -28,3 +28,12 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)): 
     return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+): 
+    return {name: age}
