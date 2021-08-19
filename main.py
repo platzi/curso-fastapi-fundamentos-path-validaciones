64ea171d2df45aaa8f87695f064832e1f4d307c4
Facundo García Martoni <thunderxforce@gmail.com>
2021-08-19 14:18:45 -0300

    automatic_path_and_query_examples

--- main.py
+++ main.py
@@ -78,12 +78,14 @@ def show_person(
         min_length=1, 
         max_length=50,
         title="Person Name",
-        description="This is the person name. It's between 1 and 50 characters"
+        description="This is the person name. It's between 1 and 50 characters",
+        example="Rocío"
         ),
     age: str = Query(
         ...,
         title="Person Age",
-        description="This is the person age. It's required"
+        description="This is the person age. It's required",
+        example=25
         )
 ): 
     return {name: age}
@@ -92,7 +94,11 @@ def show_person(
 
 @app.get("/person/detail/{person_id}")
 def show_person(
-    person_id: int = Path(..., gt=0)
+    person_id: int = Path(
+        ..., 
+        gt=0,
+        example=123
+        )
 ): 
     return {person_id: "It exists!"}
 
@@ -104,7 +110,8 @@ def update_person(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=123
     ),
     person: Person = Body(...),
     #location: Location = Body(...)
